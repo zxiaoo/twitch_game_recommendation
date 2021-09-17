9229583d6ace824e1e45a1916fbe350d870ae6f3
zxiaoo <xiaoz@terpmail.umd.edu>
2021-09-16 23:51:29 -0700

    Added MySQLDBUtil.java.

---
+++ com/myProject/twitch_recommendation/db/MySQLDBUtil.java
@@ -0,0 +1,7 @@
+package com.myProject.twitch_recommendation.db;
+
+public class MySQLDBUtil {
+    private static final String INSTANCE = "laiproject-instance.ck6werzw4tw2.us-west-1.rds.amazonaws.com";
+    private static final String PORT_NUM = "3306";
+    private static final String DB_NAME = "jupiter";
+}
