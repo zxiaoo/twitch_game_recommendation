e9c9c0f17bbf2ab5de2faaebaec7aa147b87c60a
zxiaoo <xiaoz@terpmail.umd.edu>
2021-09-18 22:17:00 -0700

    Updated MySQLDBUtil.java.

--- com/myProject/twitch_recommendation/db/MySQLDBUtil.java
+++ com/myProject/twitch_recommendation/db/MySQLDBUtil.java
@@ -1,7 +1,25 @@
 package com.myProject.twitch_recommendation.db;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class MySQLDBUtil {
     private static final String INSTANCE = "laiproject-instance.ck6werzw4tw2.us-west-1.rds.amazonaws.com";
     private static final String PORT_NUM = "3306";
-    private static final String DB_NAME = "jupiter";
+    private static final String DB_NAME = "twitchRecommendation";
+
+    public static String getMySQLAddress() throws IOException {
+        Properties prop = new Properties();
+        String propFileName = "config.properties";
+
+        InputStream inputStream = MySQLDBUtil.class.getClassLoader().getResourceAsStream(propFileName);
+        prop.load(inputStream);
+
+        String username = prop.getProperty("user");
+        String password = prop.getProperty("password");
+        return String.format("jdbc:mysql://%s:%s/%s?user=%s&password=%s&autoReconnect=true&serverTimeZone=UTC&createDatabaseIfNotExist=true",
+                            INSTANCE, PORT_NUM, DB_NAME, username, password);
+
+    }
 }
